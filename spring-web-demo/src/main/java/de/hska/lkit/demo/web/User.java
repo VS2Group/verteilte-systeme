a7e8b51b8ce8964ff9f47a88e5e4f9748c9d094e
Tobias Kerst <tobias.kerst@tobscore.com>
2016-05-29 18:19:07 +0200

    Adds exmpale user class

---
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/User.java
@@ -0,0 +1,35 @@
+package de.hska.lkit.demo.web;
+
+/**
+ * Created by Tobias Kerst on 29.05.2016.
+ */
+public class User {
+
+    private long id;
+    private String username;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
