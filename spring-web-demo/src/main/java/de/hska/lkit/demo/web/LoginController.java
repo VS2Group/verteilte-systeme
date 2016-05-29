c7f73e4ec0975db8d4293dca6e40ae78fae0bb54
Tobias Kerst <tobias.kerst@tobscore.com>
2016-05-29 18:56:25 +0200

    Receives the form data that the user entered in login screen

--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/LoginController.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Tobias Kerst on 29.05.2016.
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "login")
-    public String showLogin(@ModelAttribute User user, Model model) {
-        return "login";
-    }
-
     @RequestMapping(value = "/")
     public String showMainPage(Model model) {
         return "redirect:login";
+    }
+
+    @RequestMapping(value = "login")
+    public String showLogin(@ModelAttribute User user, Model model) {
+        if (user != null) {
+            System.out.println("User tried to log in: " + user);
+        }
+        return "login";
     }
 }
--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/User.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/User.java
@@ -32,4 +32,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String toString() {
+        return "id: {" + id + "}, username: {" + username + "}, password: {" + password + "}";
+    }
 }
