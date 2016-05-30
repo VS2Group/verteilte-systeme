d68f2570835ff7138ab00f686e81b173a59d4e5f
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2016-05-30 23:59:17 +0200

    Adds basic method for adding user to database

---
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/Persistency.java
@@ -0,0 +1,45 @@
+package de.hska.lkit.demo.web;
+
+/**
+ * Created by patrickkoenig on 29.05.16.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.support.atomic.RedisAtomicLong;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public class Persistency {
+    // inject the actual template
+
+
+    private RedisAtomicLong userid;
+    private StringRedisTemplate stringRedisTemplate;
+
+    private HashOperations<String, String, String> redisStringHashOps;
+    private SetOperations<String, String> redisStringSetOps;
+    private ListOperations<String, String> redisStringListOps;
+    private ValueOperations<String, String> redisStringValueOps;
+
+
+    @Autowired
+    public Persistency(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.userid = new RedisAtomicLong("userid", stringRedisTemplate.getConnectionFactory());
+    }
+
+
+    public boolean createUser(User user) {
+        String key = "user:" + user.getUsername();
+        redisStringHashOps.put(key, "id", userid.toString());
+        redisStringHashOps.put(key, "username", user.getUsername());
+        redisStringHashOps.put(key, "password", user.getPassword());
+
+        redisStringSetOps.add("user", key);
+        return false;
+    }
+
+}
