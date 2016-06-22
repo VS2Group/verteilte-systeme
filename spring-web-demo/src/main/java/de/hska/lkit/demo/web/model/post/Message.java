8724d40595e511108ec46a2a02975af703e7a658
Tobias Kerst <tobias.kerst@tobscore.com>
2016-06-22 12:04:51 +0200

    Changes example to support new message formatt

--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/model/post/Message.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/model/post/Message.java
@@ -1,9 +1,13 @@
-package de.hska.lkit.demo.web.model;
+package de.hska.lkit.demo.web.model.post;
 
 /**
  * Created by Tobias Kerst on 11.06.2016.
  */
 public class Message {
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 
     private String content;
 
